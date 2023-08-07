b2f1055397eec7b86379a9be18c6949261f78058
Okikiola Ojikutu <okikiolaojikutue@gmail.com>
2023-08-07 01:27:04 -0600

    New updates to all files

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".create-note");
 let notes = document.querySelectorAll(".input-box");
 
+function showNotes(){
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}showNotes()
+
+function updateStorage() {
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}//Add to browser storage
+
 createBtn.addEventListener("click", ()=>{
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
@@ -10,4 +18,27 @@ createBtn.addEventListener("click", ()=>{
     img.src= "images/delete.png";
     notesContainer.appendChild(inputBox).appendChild(img);
     
-})
+})//add me notes
+
+notesContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "IMG"){
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if(e.target.tagName === "P"){
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function(){
+                updateStorage(); 
+            } //update storage when word is added.
+        })
+    }
+    
+})//Delete Icon
+
+document.addEventListener("keydown", event => {
+    if(event.key === "Enter"){
+        document.execCommand("inserLineBreak");
+        event.preventDefault();
+    }
+})//Prevent default enter function
